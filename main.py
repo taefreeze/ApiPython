2408fecab7c8d91715557d10f166e70ae1616544
TaeFreeze <potaekingdom@hotmail.com>
2020-12-23 18:41:14 +0700

    login creds

--- main.py
+++ main.py
@@ -24,11 +24,41 @@ class NewApiList(Model):
     email = EmailType(required=True)
     name = StringType(required=True)
     password = StringType(required=True)
-
-# An instance of class User
+    
+# An instance of class NewApiList
 newList = NewApiList()
 
 # funtion to create and assign values to the instanse of class User created
+def create_user(email, username, password):
+    newList.obj_id = ObjectId()
+    newList.email = email
+    newList.name = username
+    newList.password = password
+    return dict(newList)
+
+# A method to check if the email parameter exists from the users database before validation of details
+def email_exists(email):
+    exist = True
+
+    # counts the number of times the email exists, if it equals 0 it means the email doesn't exist in the database
+    if connection.db.users.find(
+        {'email': email}
+    ).count() == 0:
+        exist = False
+        return exist
+
+# Reads user details from database and ready for validation
+def check_login_creds(email, password):
+    if not email_exists(email):
+        activeuser = connection.db.users.find(
+            {'email': email}
+        )
+        for actuser in activeuser:
+            actuser = dict(actuser)
+            # Converted the user ObjectId to str! so this can be stored into a session(how login works)
+            actuser['_id'] = str(actuser['_id'])    
+            return actuser
+
 
 
 app = FastAPI()
@@ -49,13 +79,6 @@ async def token():
     # Invalid token
         pass
     return {"userid" : userid}
-    
-def create_user(email, username, password):
-    newList.obj_id = ObjectId()
-    newList.email = email
-    newList.name = username
-    newList.password = password
-    return dict(newList)
 
 # Signup endpoint with the POST method
 @app.post("/signup/{email}/{username}/{password}")
@@ -77,6 +100,24 @@ def signup(email, username: str, password: str):
     elif user_exists == False:
         connection.db.List.insert_one(data)
         return {"message":"User Created","email": data['email'], "name": data['name'], "pass": data['password']}
+
+# Login endpoint
+@app.get("/login/{email}/{password}")
+def login(email, password):
+    def log_user_in(creds):
+        if creds['email'] == email and creds['password'] == password:
+            return {"message": creds['name'] + ' successfully logged in'}
+        else:
+            return {"message":"Invalid credentials!!"}
+    # Read email from database to validate if user exists and checks if password matches
+    logger = check_login_creds(email, password)
+    if bool(logger) != True:
+        if logger == None:
+            logger = "Invalid Email"
+            return {"message":logger}
+    else:
+        status = log_user_in(logger)
+        return {"Info":status}
 
 if __name__ == '__main__':
    uvicorn.run(app, host="0.0.0.0", port=80, debug=True) 
